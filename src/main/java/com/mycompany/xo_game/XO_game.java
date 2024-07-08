d1a031b18cc3ac5aa91208f7eae888b8c0e2cf2a
teeraphat1325 <teeraphat1325@gmail.com>
2024-07-09 00:11:48 +0700

    Innitial Project

---
+++ src/main/java/com/mycompany/xo_game/XO_game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xo_game;
+
+/**
+ *
+ * @author teeraphat
+ */
+public class XO_game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
