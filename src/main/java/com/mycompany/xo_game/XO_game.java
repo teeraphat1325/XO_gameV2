3df6006fe900abc5612bfc0c71647990836c3e8f
teeraphat1325 <teeraphat1325@gmail.com>
2024-07-11 00:21:20 +0700

    Show Table

--- src/main/java/com/mycompany/xo_game/XO_game.java
+++ src/main/java/com/mycompany/xo_game/XO_game.java
@@ -12,9 +12,19 @@ public class XO_game {
 
     public static void main(String[] args) {
         printWelcome();
+        showTable();
     }
 
     private static void printWelcome() {
         System.out.println("Welcome to my XO Game");
+    }
+
+    private static void showTable() {
+        for(int r=0;r<3;r++) {
+            for(int c=0;c<3;c++) {
+                System.out.print("- ");
+            }
+            System.out.println(" ");
+        }
     }
 }
