f79ca8d73df57a2a23706b13c3a81922b85d5cfb
teeraphat1325 <teeraphat1325@gmail.com>
2024-07-11 01:14:55 +0700

    Input RowCol

--- src/main/java/com/mycompany/xo_game/XO_game.java
+++ src/main/java/com/mycompany/xo_game/XO_game.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.xo_game;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,16 +11,19 @@ package com.mycompany.xo_game;
  */
 public class XO_game {
 
-    private static char[][] table = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+    private static char[][] table = {{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
     private static char turn = 'X';
-
+    private static Scanner sc = new Scanner(System.in);
+    private static int row;
+    private static int col;
 
     public static void main(String[] args) {
         printWelcome();
         showTable();
         showTurn();
         switchTurn();
-        
+        intpuRowCol();
+
     }
 
     private static void printWelcome() {
@@ -27,23 +31,30 @@ public class XO_game {
     }
 
     private static void showTable() {
-        for(int r=0;r<3;r++) {
-            for(int c=0;c<3;c++) {
-                System.out.print(table[r][c]+" ");
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.print(table[r][c] + " ");
             }
             System.out.println(" ");
         }
     }
 
     private static void showTurn() {
-        System.out.println("Turn "+turn);
+        System.out.println("Turn " + turn);
     }
 
     private static void switchTurn() {
-            if(turn == 'X') {
-                turn = 'O';
-            }else {
-                turn = 'X';
-            }
+        if (turn == 'X') {
+            turn = 'O';
+        } else {
+            turn = 'X';
+        }
+    }
+
+    private static void intpuRowCol() {
+        System.out.print("please enter(row,col) : ");
+        row = sc.nextInt();
+        col = sc.nextInt();
+        System.out.println(row+" "+col);
     }
 }
